1ab37323e841ddadd0ee4f320d67739d08b8bd44
Russell Schmidt <russell.schmidt@me.com>
2013-03-30 19:28:02 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resumemagic::Application.config.secret_token = 'e270ddb45cfa4c26daaea8fea9ac485f275eadd882a0ae33bd8815063006f0cef2507cc3a875a739845c6a69724dcd415e214a31b38591040efe09683f0f5a70'
